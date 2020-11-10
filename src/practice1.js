f595affce8094a18bbc227fa64cbe11c1cbf8c0f
Yafeng Wang <yafeng.wang@wyf.local>
2020-11-10 19:30:59 +0800

    利用find插入

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,7 @@
-const parseData
-export { parseData };
+const parseData = (data) => {
+  const ans = data.data.map(([name, age, gender, birthday]) =>
+    ({name, age, gender, birthday})
+  );
+  return ans;
+}
+export {parseData};
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,13 @@
-const inject
-export { inject };
+const inject = (items, sections) => {
+  let ans = [];
+  items.forEach((item, index) => {
+    const findItem = sections.find((el) => el.index === index);
+    if (findItem) {
+      ans.push(findItem.content, item);
+    } else {
+      ans.push(item);
+    }
+  });
+  return ans;
+}
+export {inject};
